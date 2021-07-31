f252af9d329d6932bedda0302f7528ac03bda4c8
“zm9” <zm9@meitu.com>
2021-07-31 21:31:15 +0800

    escape预防sql注入

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,9 +1,18 @@
-const { exec } = require("../db/mysql");
+const { exec, escape } = require("../db/mysql");
 
 function login(username, password) {
-  let sql = `select username,password from users where username='${username}' and password='${password}'`;
+  username = escape(username);
+  password = escape(password);
+  /*
+   * sql注入
+   * 输入 zhangsan'-- ，select username,password from users where username='zhangsan'-- ' and password='1233232'
+   * 那么你查询任何的username都可登录
+   * 引用sql.escape，将输入内容都转译为“”,遇到特殊字符做转译
+   */
+  let sql = `select username,password from users where username=${username} and password=${password}`;
+  console.log("sql", sql);
   return exec(sql).then((rows) => {
-    return rows[0];
+    return rows[0] || {};
   });
 }
 
